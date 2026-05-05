403cd3e3d47c4d665f42a4f3ad84bd7363a161dc
cheesecakenl <posthierin@gmail.com>
2026-05-05 23:37:59 +0200

    RMB can spawn a piggy bank;

--- Assets/Scripts/Managers/GameplayManager.cs
+++ Assets/Scripts/Managers/GameplayManager.cs
@@ -10,6 +10,7 @@ public class GameplayManager : MonoBehaviour
 
     [SerializeField] private Texture2D mouseTexture;
     [SerializeField] private GameObject chestPrefab;
+    [SerializeField] private GameObject piggyBankPrefab;
 
     private Vector2 clickHotspot;
 
@@ -76,7 +77,19 @@ public class GameplayManager : MonoBehaviour
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePos.z = 0f;
 
-        GameObject clone = Instantiate(chestPrefab, mousePos, Quaternion.identity);
+        GameObject prefab = null;
+        int idx = UnityEngine.Random.Range(1, 3);
+
+        if (idx == 1)
+        {
+            prefab = chestPrefab;
+        }
+        if (idx == 2)
+        {
+            prefab = piggyBankPrefab;
+        }
+
+        GameObject clone = Instantiate(prefab, mousePos, Quaternion.identity);
     }
 
     private void HandleOnCoinPickup(double amount)
